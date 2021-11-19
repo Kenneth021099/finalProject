9204dfc06842931ac0882225755ce98e9229d17f
kenneth <kennethmanalac021099@gmail.com>
2021-11-20 00:50:47 +0800

    addded my part

--- src/main/java/com/example/finalProject/FinalProjectApplication.java
+++ src/main/java/com/example/finalProject/FinalProjectApplication.java
@@ -1,13 +1,35 @@
 package com.example.finalProject;
 
+import com.example.finalProject.model.Student;
+import com.example.finalProject.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalProjectApplication {
+public class FinalProjectApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinalProjectApplication.class, args);
 	}
 
+	@Autowired
+	private StudentRepository studentRepository;
+	@Override
+	public void run(String... args) throws Exception {
+		Student student = new Student();
+		student.setFirstName("Kenneth");
+		student.setLastName("Mañalac");
+		student.setAddress("Candaba");
+		student.setEmailId("kennethmanalac@gmail.com");
+		studentRepository.save(student);
+
+		Student student1 = new Student();
+		student1.setFirstName("iron");
+		student1.setLastName("Man");
+		student1.setAddress("newyork");
+		student1.setEmailId("ironman@gmail.com");
+		studentRepository.save(student1);
+	}
 }
---
+++ src/main/java/com/example/finalProject/controller/StudentController.java
@@ -0,0 +1,24 @@
+package com.example.finalProject.controller;
+
+import com.example.finalProject.model.Student;
+import com.example.finalProject.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudent(){
+        return studentRepository.findAll();
+
+    }
+}
